9a6b21f64704ae350beb338319669e627ecd6e93
sai dorababu <khsd118194@gmail.com>
2024-03-20 22:51:30 +0530

    first commit

---
+++ newparsetree/file2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+int main(int a){
+    int a = 5,d;
+    cin>>a; 
+    char c = 'c';
+    for(i=0;i<10;i++){
+        a = a + 1;
+    }
+    if(b) a = 10;
+    else {
+        cout<<"2";
+    }
+    return 0;
+}
